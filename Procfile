197d04b34107b3712d18766e2806d4f501e89e4e
Bryce Berry <bryceberry4710@gmail.com>
2026-08-29 17:14:38 -0400

    Small bug changes before hosting!

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn acreiq.wsgi
--- acreiq/settings.py
+++ acreiq/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&rsu7!b6%w3ody&vy4$@^@ls^&0vvo58h!=1#+=mjq&5!((#zp'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
